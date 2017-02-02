edd03248d0f041e070e14ccbb8d08f72be529175
Nuchaz <nuchaz.ipod@gmail.com>
2017-02-01 20:24:52 -0500

    trying something else

--- client/app/factories/listings.client.factory.js
+++ client/app/factories/listings.client.factory.js
@@ -2,23 +2,23 @@ angular.module('listings').factory('Listings', ['$http',
   function($http) {
     var methods = {
       getAll: function() {
-        return $http.get('https://bootcampsix.herokuapp.com/listings');
+        return $http.get('https://localhost:8080/api/listings');
       },
 
       create: function(listing) {
-        return $http.post('https://bootcampsix.herokuapp.com/listings', listing);
+        return $http.post('https://localhost:8080/api/listings', listing);
       }, 
 
       read: function(id) {
-        return $http.get('https://bootcampsix.herokuapp.com/listings/' + id);
+        return $http.get('https://localhost:8080/api/listings/' + id);
       }, 
 
       update: function(id, listing) {
-        return $http.put('https://bootcampsix.herokuapp.com/listings/' + id, listing);
+        return $http.put('https://localhost:8080/api/listings/' + id, listing);
       }, 
 
       delete: function(id) {
-        return $http.delete('https://bootcampsix.herokuapp.com/listings/' + id);
+        return $http.delete('https://localhost:8080/api/listings/' + id);
       }
     };
 
