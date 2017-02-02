3c4fd4ede4d23d61d5859abec3359049d6db7b8b
Nuchaz <nuchaz.ipod@gmail.com>
2017-02-01 20:35:55 -0500

    another experiement

--- client/app/factories/listings.client.factory.js
+++ client/app/factories/listings.client.factory.js
@@ -2,23 +2,23 @@ angular.module('listings').factory('Listings', ['$http',
   function($http) {
     var methods = {
       getAll: function() {
-        return $http.get('https://localhost:8080/api/listings');
+        return $http.get('http://23.21.203.159:8080/api/listings');
       },
 
       create: function(listing) {
-        return $http.post('https://localhost:8080/api/listings', listing);
+        return $http.post('http://23.21.203.159:8080/api/listings', listing);
       }, 
 
       read: function(id) {
-        return $http.get('https://localhost:8080/api/listings/' + id);
+        return $http.get('http://23.21.203.159:8080/api/listings/' + id);
       }, 
 
       update: function(id, listing) {
-        return $http.put('https://localhost:8080/api/listings/' + id, listing);
+        return $http.put('http://23.21.203.159:8080/api/listings/' + id, listing);
       }, 
 
       delete: function(id) {
-        return $http.delete('https://localhost:8080/api/listings/' + id);
+        return $http.delete('http://23.21.203.159:8080/api/listings/' + id);
       }
     };
 
