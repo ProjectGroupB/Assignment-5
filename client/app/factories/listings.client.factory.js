9780e2873a1eeb0f1113480f9d4edb1af7232910
Nuchaz <nuchaz.ipod@gmail.com>
2017-02-01 20:48:34 -0500

    I think this is where it needs to be
    still hvaing issues connecting to database, but it is almost there I think.

--- client/app/factories/listings.client.factory.js
+++ client/app/factories/listings.client.factory.js
@@ -2,23 +2,23 @@ angular.module('listings').factory('Listings', ['$http',
   function($http) {
     var methods = {
       getAll: function() {
-        return $http.get('http://localhost:8080/api/listings');
+        return $http.get('http://bootcampsix.herokuapp.com/listings');
       },
 
       create: function(listing) {
-        return $http.post('http://localhost:8080/api/listings', listing);
+        return $http.post('http://bootcampsix.herokuapp.com/listings', listing);
       }, 
 
       read: function(id) {
-        return $http.get('http://localhost:8080/api/listings/' + id);
+        return $http.get('http://bootcampsix.herokuapp.com/listings/' + id);
       }, 
 
       update: function(id, listing) {
-        return $http.put('http://localhost:8080/api/listings/' + id, listing);
+        return $http.put('http://bootcampsix.herokuapp.com/listings/' + id, listing);
       }, 
 
       delete: function(id) {
-        return $http.delete('http://localhost:8080/api/listings/' + id);
+        return $http.delete('http://bootcampsix.herokuapp.com/listings/' + id);
       }
     };
 
